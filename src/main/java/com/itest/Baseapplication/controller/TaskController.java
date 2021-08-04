517f568f680249234e738a571de8ab7c3032719b
Himanshu Tripathi(himanshu.tripathi01) <himanshu.tripathi01@infosys.com>
2021-08-04 13:35:22 +0530

    Add cross origin

--- src/main/java/com/itest/Baseapplication/controller/TaskController.java
+++ src/main/java/com/itest/Baseapplication/controller/TaskController.java
@@ -6,10 +6,7 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.LinkedHashMap;
@@ -17,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/Tasks/")
+@CrossOrigin(origins="http://localhost:4200")
 public class TaskController {
 
     @Autowired
