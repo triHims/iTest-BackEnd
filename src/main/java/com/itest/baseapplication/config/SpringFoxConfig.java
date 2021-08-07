0089336931b9c91b896f985968c0b77dbcb26250
Himanshu Tripathi(himanshu.tripathi01) <himanshu.tripathi01@infosys.com>
2021-08-07 20:39:06 +0530

    Spring Auth with swagger.

--- src/main/java/com/itest/baseapplication/config/SpringFoxConfig.java
+++ src/main/java/com/itest/baseapplication/config/SpringFoxConfig.java
@@ -2,20 +2,38 @@ package com.itest.baseapplication.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.SecurityScheme;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 public class SpringFoxConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(metaData())
+                .securitySchemes(securitySchemes())
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.itest.baseapplication.controller"))
                 .paths(PathSelectors.any())
-                .build()
-                .pathMapping("/");
+                .build();
+    }
+
+    private ApiInfo metaData() {
+        return new ApiInfoBuilder().title("iTest Backend")
+                .description("\"Springboot service for itest ux tester.\"").version("1.0.0").build();
+    }
+    private static List <SecurityScheme> securitySchemes() { // required to enable authorization
+        List <SecurityScheme> token = new ArrayList<>();
+        token.add(new ApiKey("Bearer", "Authorization", "header"));
+        return token;
     }
 }
