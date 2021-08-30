46d5423f3e050f9d21dd6d37eb112d0195ea28d6
Himanshu Tripathi(himanshu.tripathi01) <himanshu.tripathi01@infosys.com>
2021-08-30 10:15:14 +0530

    use @Param to overcome jpql error

--- src/main/java/com/itest/baseapplication/repository/ProjectTesterMapperRepo.java
+++ src/main/java/com/itest/baseapplication/repository/ProjectTesterMapperRepo.java
@@ -4,6 +4,7 @@ package com.itest.baseapplication.repository;
 import com.itest.baseapplication.entity.ProjectTesterMapper;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,5 +12,5 @@ import java.util.List;
 @Repository
 public interface ProjectTesterMapperRepo extends JpaRepository<ProjectTesterMapper,Integer> {
 	@Query("Select projectId from ProjectTesterMapper where testerId=:testerId")
-	List<String> findProjectByTesterId(int testerId);
+	List<String> findProjectByTesterId(@Param("testerId") int testerId);
 }
